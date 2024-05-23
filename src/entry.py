da2b6688531b631fd834df49c9402f47a56ae39f
Leo Douglas <douglarek@gmail.com>
2024-05-23 12:39:33 +0800

    initial commit

---
+++ src/entry.py
@@ -0,0 +1,12 @@
+from js import fetch, Request, Response, URL
+
+
+async def on_fetch(request, env):
+    url = URL.new(request.url)
+    url.hostname = "api.openai.com"
+    request = Request.new(url.toString(), request)
+    api_key = request.headers.get("Authorization")
+    if api_key == "Bearer " + env.TOKEN:
+        request.headers["Authorization"] = "Bearer " + env.OPENAI_API_KEY
+    response = await fetch(request)
+    return Response.new(response.body, response)
